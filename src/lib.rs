7a67d981f19385548e36bd126ef5bafe443229f0
kjgorman <kieran@kjgorman.com>
2015-12-16 19:19:17 -0500

    :raised_hands: basic implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,47 @@
 #![crate_name = "editdistancewf"]
 #![crate_type = "lib"]
 
-pub fn distance<T, I: Iterator<Item=T>>(left: I, right: I) -> usize {
-    0
+fn materialise<T, I: Iterator<Item=T>>(iter: I) -> Vec<T> {
+    let mut dest = Vec::new();
+    dest.extend(iter);
+
+    dest
 }
+
+pub fn distance<T, I: Iterator<Item=T>>(left_stream: I, right_stream: I) -> usize
+    where T: Eq {
+        let left  = materialise(left_stream);
+        let right = materialise(right_stream);
+
+        let width = left.len() + 1;
+        let depth = right.len() + 1;
+
+        let mut matrix = vec![vec![0; depth]; width];
+
+        for i in 0..width {
+            matrix[i][0] = i;
+        }
+
+        for j in 0..depth {
+            matrix[0][j] = j;
+        }
+
+        for j in 1..depth {
+            for i in 1..width {
+                // these elements match, propagate current distances
+                if left[i-1] == right[j-1] {
+                    matrix[i][j] = matrix[i-1][j-1];
+                } else {
+                    matrix[i][j] = *[
+                        matrix[i-1][j] + 1,    // deletion
+                        matrix[i][j-1] + 1,    // insertion
+                        matrix[i-1][j-1] + 1]  // substitution
+                        .iter()
+                        .min()
+                        .unwrap();
+                }
+            }
+        }
+
+        matrix[width-1][depth-1]
+    }
--- tests/basics.rs
+++ tests/basics.rs
@@ -7,13 +7,35 @@ mod basic_distances {
     #[test]
     pub fn two_identical_sequences_have_a_distance_of_zero () {
         let left  = "left";
-        let right = "right";
+        let right = "left";
 
         assert_eq!(wf::distance(left.chars(), right.chars()), 0)
     }
 
     #[test]
-    pub fn simple_differences () {
-        assert_eq!(wf::distance("left".chars(), "lend".chars()), 2)
+    pub fn simple_insert () {
+        assert_eq!(wf::distance("left".chars(), "lefty".chars()), 1)
+    }
+
+    #[test]
+    pub fn simple_delete () {
+        assert_eq!(wf::distance("left".chars(), "lef".chars()), 1)
+    }
+
+    #[test]
+    pub fn simple_modification () {
+        assert_eq!(wf::distance("left".chars(), "lefy".chars()), 1)
+    }
+
+    #[test]
+    pub fn empty_comparisons () {
+        assert_eq!(wf::distance("".chars(), "foo".chars()), 3);
+        assert_eq!(wf::distance("foo".chars(), "".chars()), 3);
+    }
+
+    #[test]
+    pub fn non_string_comparisons () {
+        assert_eq!(wf::distance([1,2,3].iter(), [1,2,3].iter()), 0);
+        assert_eq!(wf::distance([1,2].iter(), [1,2,3].iter()), 1);
     }
 }
